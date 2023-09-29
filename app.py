1e94643a2216a95d179c7adf314b2625a393784d
sgomezp <sgomezp@gmail.com>
2023-09-29 10:05:29 +0200

    feat: testing with redis

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import json
 from pymongo import MongoClient
 import dotenv
+import redis
 
 dotenv.load_dotenv()
 
@@ -20,16 +21,49 @@ app = faust.App(
     value_serializer='json',
 )
 
+# Configura la conexión a Redis (cambia la dirección y el puerto si es necesario)
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+
 class NetData(faust.Record):
     address: str
     IPv4: str
-    
-topic = app.topic('probando', value_type=NetData)
+
+
+class PersonalData(faust.Record):
+    name: str
+    last_name: str
+    sex: str
+    telfnumber: str
+    passport: str
+    email: str
+
+
+topic = app.topic('probando')
+
 
 @app.agent(topic)
 async def process_data(messages):
     async for message in messages:
         print(message)
+
+        # Verifica la clase del mensaje y almacena en Redis en consecuencia
+        if isinstance(message, NetData):
+            # Almacena los datos de NetData en Redis
+            redis_key = f'netdata_{message.address}'  # Clave única basada en la dirección
+            redis_value = message.asdict()  # Convierte el mensaje a un diccionario
+        elif isinstance(message, PersonalData):
+            # Almacena los datos de PersonalData en Redis
+            redis_key = f'personaldata_{message.name}'  # Clave única basada en el nombre
+            redis_value = message.asdict()  # Convierte el mensaje a un diccionario
+        else:
+            print(f'Mensaje de tipo desconocido: {message}')
+            continue  # Ignora mensajes de tipos desconocidos
+
+        # Guarda el mensaje en Redis
+        redis_client.set(redis_key, json.dumps(redis_value))
+        print(f'Mensaje guardado en Redis: {redis_key}')
+
 
 if __name__ == '__main__':
     app.main()
