2a3bf60cad41cd390989ce4a33d86b071886ca00
sgomezp <sgomezp@gmail.com>
2023-10-11 09:58:09 +0200

    feat: Added function that join common objects

--- app.py
+++ app.py
@@ -32,48 +32,54 @@ app = faust.App(
     value_serializer='json',
 )
 
-# Configura la conexión a Redis (cambia la dirección y el puerto si es necesario)
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+probando_topic = app.topic('probando')
 
 
-class NetData(faust.Record):
-    address: str
-    IPv4: str
+@app.agent(probando_topic)
+async def process_probando(probando_stream):
+    counter = 0
+    counter_insertions = 0
+    async for data in probando_stream:
+        # print(f"Received Data: {data}")
+        counter += 1
+        print(f"Counter: {counter}")
+        try:
+            passport = data.get('passport')
+            fullname = data.get('fullname')
+            address = data.get('address')
+            name = data.get('name')
+            lastname = data.get('last_name')
+            sex = data.get('sex')
+
+            if passport:
+                if name:
+                    data['sex'] = sex[0] if sex else 'Not specified'
+                    data['fullname'] = f"{name} {lastname}"
+                    del data['name']
+                    del data['last_name']
+                redis_client.hset(f'passport: {passport}', mapping=data)
+                if redis_client.hlen(f'passport: {passport}') > 5:
+                    hash_data = redis_client.hgetall(f'passport: {passport}')
+                    new_fullname = hash_data.get(b'fullname').decode('utf-8')
+                    new_fullname_hash = f'fullname: {new_fullname}'
+                    redis_client.hset(new_fullname_hash, mapping=hash_data)
+                    redis_client.delete(f'passport: {passport}')
+            elif address:
+                redis_client.hset(f'address: {address}', mapping=data)
+                if redis_client.hlen(f'address: {address}') > 3:
+                    hash_data = redis_client.hgetall(f'address: {address}')
+                    new_fullname = hash_data.get(b'fullname').decode('utf-8')
+                    new_fullname_hash = f'fullname: {new_fullname}'
+                    redis_client.hset(new_fullname_hash, mapping=hash_data)
+                    redis_client.delete(f'address: {address}')
+            elif fullname:
+                redis_client.hset(f'fullname: {fullname}', mapping=data)
 
 
-class PersonalData(faust.Record):
-    name: str
-    last_name: str
-    sex: str
-    telfnumber: str
-    passport: str
-    email: str
 
-
-topic = app.topic('probando')
-
-
-@app.agent(topic)
-async def process_data(messages):
-    async for message in messages:
-        print(message)
-
-        # Verifica la clase del mensaje y almacena en Redis en consecuencia
-        if isinstance(message, NetData):
-            # Almacena los datos de NetData en Redis
-            redis_key = f'netdata_{message.address}'  # Clave única basada en la dirección
-            redis_value = message.asdict()  # Convierte el mensaje a un diccionario
-        elif isinstance(message, PersonalData):
-            # Almacena los datos de PersonalData en Redis
-            redis_key = f'personaldata_{message.name}'  # Clave única basada en el nombre
-            redis_value = message.asdict()  # Convierte el mensaje a un diccionario
-        else:
-            print(f'Mensaje de tipo desconocido: {message}')
-            continue  # Ignora mensajes de tipos desconocidos
-
-        # Guarda el mensaje en Redis
-        redis_client.set(redis_key, json.dumps(redis_value))
-        print(f'Mensaje guardado en Redis: {redis_key}')
+        except json.JSONDecodeError as e:
+            print(f"Error al decodificar JSON: {e}, Data: {data}")
 
 
 if __name__ == '__main__':
