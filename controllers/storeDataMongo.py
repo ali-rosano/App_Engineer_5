89862b876c9bfcddf12439c11d6e77d8452ba838
Camilo Andres Vanegas <kamilo_vanegas@hotmail.com>
2023-10-12 19:40:17 +0200

    refactor: Change success message when data is stored in Mongo and SQL

--- controllers/storeDataMongo.py
+++ controllers/storeDataMongo.py
@@ -2,4 +2,4 @@ from config.mongo_database import collection
 
 async def store_data_in_mongo(data):
     collection.insert_many(data)
-    print('Data stored successfully 🚀')
+    print('Data stored successfully in Mongo and SQL 🚀')
